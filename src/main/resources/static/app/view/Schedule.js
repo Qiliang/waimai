5014850c521281ec0bf901c8fc511ed85baf8295
xiaoql@yeah.net <xiaoql@yeah.net>
2017-07-31 10:29:59 +0800

    持久化

--- src/main/resources/static/app/view/Schedule.js
+++ src/main/resources/static/app/view/Schedule.js
@@ -1,13 +1,8 @@
 Ext.define('Kits.view.Schedule', {
-    extend: 'Ext.grid.Panel',
+    extend: 'Ext.panel.Panel',
     title: '调度管理',
     store: Ext.create('Kits.store.User'),
-    columns: [
-        {text: 'id', dataIndex: 'id'},
-        {text: '名称', dataIndex: 'name'},
-        {text: '登录名', dataIndex: 'loginName'},
-        {text: '登录密码', dataIndex: 'loginPassword'},
-        {text: '状态', dataIndex: 'alive'},
-        {text: '角色', dataIndex: 'role'}
-    ]
+    html: '<div id="allmap"></div>',
+
+
 });
